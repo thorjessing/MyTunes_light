f7ef02a58f399cda838714a1e2b71de1c4157057
Lykke Bernberg <lykber01@easv365.dk>
2024-12-03 14:24:09 +0100

    sat sange på playlister og vi kan også se sange i div. playlister

--- src/main/java/dk/easv/MyTunes_light/GUI/Controller/MyTunesController.java
+++ src/main/java/dk/easv/MyTunes_light/GUI/Controller/MyTunesController.java
@@ -1,7 +1,10 @@
 package dk.easv.MyTunes_light.GUI.Controller;
 
+import dk.easv.MyTunes_light.BE.Playlist;
 import dk.easv.MyTunes_light.BE.Song;
+import dk.easv.MyTunes_light.GUI.Model.PlaylistModel;
 import dk.easv.MyTunes_light.GUI.Model.SongModel;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,34 +20,120 @@ public class MyTunesController implements Initializable {
     @FXML
     private TableView<Song> tblViewSongs;
     @FXML
-    private TableColumn<Integer, Song> songDurationCol;
+    private TableColumn<Song, String> songDurationCol;
     @FXML
-    private TableColumn<String, Song> songGenreCol;
+    private TableColumn<Song, String> songGenreCol;
     @FXML
-    private TableColumn<String, Song> songArtistCol;
+    private TableColumn<Song, String> songArtistCol;
     @FXML
-    private TableColumn<String, Song> songTitleCol;
+    private TableColumn<Song, String> songTitleCol;
+
+    @FXML
+    private TableView<Playlist> tblViewPlaylist; // Korrekt Playlist TableView
+
+
 
     private SongModel songModel;
-    private boolean isPlaying = false;
-    private ObservableList observableListSongs;
+    private PlaylistModel playlistModel;
+    private ObservableList<Song> observableListSongs;
+    private ObservableList<Playlist> observableListPlaylists;
+    private ObservableList<Song> observablePlaylistSongs;
+
+    @FXML
+    private TableColumn<Playlist, String> playlistNameCol;
+    @FXML
+    private TableColumn<Playlist, Integer> playlistSongsCol;
+    @FXML
+    private TableColumn<Playlist, String> playlistDurationCol;
+    @FXML
+    private ListView<Song> listViewPlaylistSongs;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
             observableListSongs = songModel.getSongs();
+            observableListPlaylists = playlistModel.getPlaylists();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
+        observablePlaylistSongs = FXCollections.observableArrayList();
+
+        populateSongs();
+        populatePlaylists();
+    }
+
+    public MyTunesController() throws Exception {
+        songModel = new SongModel();
+        playlistModel = new PlaylistModel(); // Tilføjet initialisering
+    }
+
+    //definere kolonner typer og smider data i songs
+    private void populateSongs() {
         songTitleCol.setCellValueFactory(new PropertyValueFactory<>("name"));
         songGenreCol.setCellValueFactory(new PropertyValueFactory<>("genre"));
-        songDurationCol.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        songDurationCol.setCellValueFactory(new PropertyValueFactory<>("length"));
         songArtistCol.setCellValueFactory(new PropertyValueFactory<>("artist"));
         tblViewSongs.setItems(observableListSongs);
     }
 
-    public MyTunesController() throws Exception {
-        tblViewSongs = new TableView<>();
-        songModel = new SongModel();
+    //definerer kolonner typer og smider data i playlister
+    private void populatePlaylists() {
+        playlistNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        playlistSongsCol.setCellValueFactory(new PropertyValueFactory<>("songCount"));
+        playlistDurationCol.setCellValueFactory(new PropertyValueFactory<>("duration"));
+
+        tblViewPlaylist.setItems(observableListPlaylists);
+
+        listViewPlaylistSongs.setCellFactory(lv -> new ListCell<>() {
+            @Override
+            protected void updateItem(Song song, boolean empty) {
+                super.updateItem(song, empty);
+                setText(empty || song == null ? null : song.getArtist() + " - " + song.getName());
+            }
+        });
+
+
+        registerPlaylistChange();
+    }
+
+    private void registerPlaylistChange() {
+        tblViewPlaylist.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, playlist) -> {
+            if (playlist != null) {
+                observablePlaylistSongs.setAll(playlist.getSongs());
+                listViewPlaylistSongs.setItems(observablePlaylistSongs);
+            }
+        });
+
+    }
+
+
+    @FXML
+    private void handleCreatePlaylist() {
+        String playlistName = "New Playlist"; // Prompt kan tilføjes her.
+        Playlist newPlaylist = new Playlist(playlistName);
+        playlistModel.addPlaylist(newPlaylist);
+    }
+
+    @FXML
+    private void handleAddSongToPlaylist() {
+        Song selectedSong = tblViewSongs.getSelectionModel().getSelectedItem();
+        Playlist selectedPlaylist = tblViewPlaylist.getSelectionModel().getSelectedItem();
+
+        if (selectedSong != null && selectedPlaylist != null) {
+            playlistModel.addSongToPlaylist(selectedPlaylist, selectedSong);
+        }
+    }
+
+    @FXML
+    private void handleRemoveSongFromPlaylist() {
+        Song selectedSong = tblViewSongs.getSelectionModel().getSelectedItem();
+        Playlist selectedPlaylist = tblViewPlaylist.getSelectionModel().getSelectedItem();
+
+        if (selectedSong != null && selectedPlaylist != null) {
+            playlistModel.removeSongFromPlaylist(selectedPlaylist, selectedSong);
+        }
     }
 }
+
+
