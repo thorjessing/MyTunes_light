dc743405de3f9110a0c014f98f65e7cbe3c68dda
Lykke Bernberg <lykber01@easv365.dk>
2024-12-09 12:06:23 +0100

    fjernet ligegyldige klasser

--- src/main/java/dk/easv/MyTunes_light/DAL/ISongDataAccess.java
+++
@@ -1,6 +0,0 @@
-package dk.easv.MyTunes_light.DAL;
-
-public interface ISongDataAccess {
-
-    //ADD CRUD METHODS TO INTERFACE
-}
--- src/main/java/dk/easv/MyTunes_light/DAL/MyTunes_lightDAO_db.java
+++
@@ -1,6 +0,0 @@
-package dk.easv.MyTunes_light.DAL;
-
-import java.io.IOException;
-
-public class MyTunes_lightDAO_db {
-}
